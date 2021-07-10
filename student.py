c383ba3f5afc9bb97a9ddd29830099d1551d2b2b
incetona <incetona@gmail.com>
2021-07-11 00:46:09 +0200

    student class modified

--- student.py
+++ student.py
@@ -1,37 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
 class Student:
-    """ This is a class for students of Golestan website. 
-    their name, credential and public data are here. """
+    """ This is a dataclass for students of Golestan website. 
+    Their educational and public data are stored here. """
 
-    def __init__(self, firstName, lastName, studentNumber) -> None:
-        self.firstName = firstName
-        self.lastName = lastName
-        self.studentNumber = studentNumber
-    
-    def __str__(self) -> str:
-        return self.firstName + " " + self.lastName
-
-    # @property
-    # def firstName(self):
-    #     return self.firstName
-
-    # @firstName.setter
-    # def firstName(self, value):
-    #     self.firstName = value
-
-    # @firstName.getter
-    # def firstName(self):
-    #     return self.firstName
-
-    # @property
-    # def lastName(self):
-    #     return self.lastName
-
-    # @lastName.setter
-    # def lastName(self, value):
-    #     self.lastName = value
-    
-
-ehsan = Student('Ehsan', 'Attar', '94432027')
-# ehsan.firstName('Ehsan')
-# ehsan.lastName('Attar')
-print(ehsan)
+    university: str
+    faculty: str
+    department: str
+    subject: str
+    degree: str
+    courseType: str
