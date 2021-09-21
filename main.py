0fc61df33b0489f65247d2ff7d7540c5651e25fe
incetona <incetona@gmail.com>
2021-09-21 03:25:49 +0200

    *New desgin pattern*

--- main.py
+++ main.py
@@ -1,29 +1,201 @@
-from selenium.common.exceptions import ElementNotInteractableException, NoSuchFrameException, NoSuchWindowException
-from browser import Browser
-from browser import GOLESTAN_PASSWORD, GOLESTAN_USERNAME
+from selenium.common.exceptions import NoSuchFrameException, NoSuchWindowException, \
+    NoAlertPresentException
+from telegram import Update, ForceReply
+from telegram.ext import CallbackQueryHandler, ConversationHandler, MessageHandler, Filters, CommandHandler, Updater, CallbackContext
+from telegramBot import start_menu_new_user, golestan_menu, TelegramBot, KeyboardButton, ReplyKeyboardMarkup, save_credentials, \
+    unis_menu, provinces_menu, uni_generator, uni_json_list
 import os
+import json
+from dotenv import load_dotenv
+from db import Mongo
 
-browser = Browser(headless=False)
+USERNAME, PASSWORD, CAPTCHA = range(3)
+Mongo.init()
+load_dotenv()
+telegram = TelegramBot()
 
-def login():
+API_KEY = os.environ.get('API_KEY')
 
-    browser.get_url()
-    browser.captcha()
-    browser.enter_username_password(GOLESTAN_USERNAME, GOLESTAN_PASSWORD)
-    while True:
-        try:
-            browser.submit_username_password()
-            browser.go_to_login_error_frame()
-            browser.check_error()
-        except(NoSuchFrameException, NoSuchWindowException, ElementNotInteractableException):
-            break
+class Golestan:
+    username = None
+    password = None
+    captcha = None
+    telegram_username = None
+# Start Command
+def start(update: Update, context: CallbackContext) -> None:
+    if Mongo.is_username_in_db(update.message.from_user.username):
+        Golestan.telegram_username = update.message.from_user.username
+        # keyboard = [[KeyboardButton("/login")], [KeyboardButton("/start")] ]
+        keyboard = [['/start'], ['/login']]
+        markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=False)
+        update.message.reply_text(text="خوش آمدید. بر روی Login بزنید.", reply_markup=markup)
+    else:
+        start_menu_new_user(update)
 
-def main():
-    
-    login()
-    browser.userInputMenuNumber()
-    browser.go_to_menu()
-    browser.driver.quit()
 
-if __name__ == "__main__":
+def login(update: Update, context: CallbackContext):
+
+    telegram.get_uni_page(update, context)
+    try:
+        alert = telegram.browser.driver.switch_to_alert()
+        alert.accept()
+
+        update.message.reply_text('شناسه کاربری:', reply_markup=ForceReply())
+        return USERNAME
+
+    except NoAlertPresentException:
+        update.message.reply_text('شناسه کاربری:', reply_markup=ForceReply())
+        return USERNAME
+
+
+def enter_user_password():
+    telegram.browser.enter_username_password(Golestan.username, Golestan.password)
+
+def enter_captcha():
+    telegram.browser.enter_captcha(Golestan.captcha)
+
+def save_golestan_username(update: Update, context: CallbackContext) -> int:
+    Golestan.username = update.message.text
+    update.message.reply_text('گذرواژه:', reply_markup=ForceReply())
+    return PASSWORD
+
+
+def save_golestan_password(update: Update, context: CallbackContext) -> int:
+    Golestan.password = update.message.text
+    telegram.captcha(update, context)
+    return CAPTCHA
+
+
+def save_golestan_captcha(update: Update, context: CallbackContext) -> int:
+    Golestan.captcha = update.message.text
+    enter_user_password()
+    enter_captcha()
+    telegram.browser.submit_username_password()
+    try:
+        telegram.browser.go_to_login_error_frame()
+        check_error(update, context)
+    except (NoSuchFrameException, NoSuchWindowException, NoSuchFrameException):
+        pass
+    telegram.browser.go_to_frame('Faci2', 'Master', 'Form_Body')
+    if telegram.browser.driver.find_element_by_id('F51851'):
+        save_credentials(update, context)
+        return ConversationHandler.END
+
+
+def check_error(update: Update, context: CallbackContext):
+
+    error_element = telegram.browser.driver.find_element_by_id('errtxt')
+    error_message = error_element.get_attribute('title')
+    if error_message == "لطفا كد امنيتي را به صورت صحيح وارد نماييد":
+        context.bot.send_message(update.message.chat_id, error_message)
+        return telegram.captcha(update, context)
+
+    if error_message == "کد1 : شناسه کاربري يا گذرواژه اشتباه است.":
+        context.bot.send_message(update.message.chat_id, error_message)
+        keyboard = [['/login']]
+        markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=False)
+        update.message.reply_text(text='بر روی Login بزنید.', reply_markup=markup)
+        return
+
+    if error_message == "لطفا صبر کنيد.":
+        telegram.browser.driver.implicitly_wait(1)
+        check_error(update, context)
+
+
+def cancel(update: Update, context: CallbackContext):
+    pass
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+
+    # NEW USER
+    # Start Inlinekeyboard -> CHOOSE PROVINCE
+    if query.data == 'choose_province_btn':
+        provinces_menu(query)
+    # Choose Province BACK Inlinekeyboard -> Start
+    if query.data == 'province_menu_back_btn':
+        start_menu_new_user(update, back=True)
+    # Province Inlinekeyboard -> CHOOSE UNI
+    for city in list(uni_json_list.keys()):
+        if query.data == city:
+            return unis_menu(query, city)
+    # Choose Uni BACK Inlinekeyboard -> Province
+    if query.data == 'uni_menu_back_btn':
+        unis_menu(query, back=True)
+    # Select Uni
+    uni_name_list = uni_generator()
+    for uni in uni_name_list:
+        for uni_name, link in uni.items():
+            if query.data == uni_name[0:28]:
+                telegram.UNIVERSITY_NAME = uni_name
+                telegram.UNIVERSITY_LINK = link
+                telegram.TELEGRAM_USERNAME = query.from_user.username
+                query.bot.answer_callback_query(query.id, text='نام دانشگاه ذخیره شد.', show_alert=True,
+                                                cache_time=0)
+
+                telegram.save_to_db()
+                query.bot.send_message(query.message.chat_id, "دوباره استارت کنید")
+                return
+    # USER MENU
+    # if query.data == "login":
+    #     query.bot.send_message(text='اطلاعات کاربری خود در سامانه گلستان را وارد کنید.',
+    #                            chat_id=query.message.chat_id)
+    #     self.username_password(query)
+
+    if query.data == "week_schedule":
+        query.bot.send_message(query.message.chat_id, "لطفا صبر کنید.")
+        telegram.browser.go_to_menu('78')
+        if os.path.isfile('week.png'):
+            query.bot.send_photo(chat_id=query.message.chat_id, photo=open('week.png', 'rb'))
+        if os.path.isfile('week.txt'):
+            with open('week.txt', 'r') as textFile:
+                text = textFile.read()
+            query.bot.send_message(chat_id=query.message.chat_id, text=text)
+
+    # if query.data == "golestan_menu_back":
+    #     start_menu(update, back=True)
+
+    if query.data == "yes":
+        Mongo.COL.find_one_and_update(
+            {"telegramUsername": Golestan.telegram_username},
+            {"$set": {"golestanUsername": Golestan.username,
+                      "golestanPassword": Golestan.password}})
+        query.bot.answer_callback_query(query.id, text='اطلاعات کاربری شما ذخیره شد.',
+                                        show_alert=True)
+        golestan_menu(query)
+
+    if query.data == "no":
+        golestan_menu(query)
+
+conv_handler = ConversationHandler(allow_reentry=True,
+    entry_points=[CommandHandler('login', login)],
+    states={
+        USERNAME: [MessageHandler(Filters.text, save_golestan_username)],
+        PASSWORD: [MessageHandler(Filters.text, save_golestan_password)],
+        CAPTCHA: [MessageHandler(Filters.text, save_golestan_captcha)],
+    },
+    fallbacks=[CommandHandler('cancel', cancel)],
+)
+
+
+
+def main() -> None:
+    """Run the bot."""
+    updater = Updater(API_KEY)
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    # updater.dispatcher.add_handler(CommandHandler('help', help_cmd))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(conv_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
     main()
--- telegramBot.py
+++ telegramBot.py
@@ -1,28 +1,20 @@
 import logging
 import os
 import json
+from enum import Enum, auto
+from db import Mongo
 import numpy as np
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ForceReply
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
-import pymongo
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, ForceReply, KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext,
+                          CallbackQueryHandler)
+
 from browser import Browser, EC, wait, By
 from browser import NoSuchFrameException, NoSuchWindowException, ElementNotInteractableException, NoSuchElementException
-from dotenv import load_dotenv
-
-load_dotenv()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-API_KEY = os.environ.get('API_KEY')
-
-client = pymongo.MongoClient('localhost', 27017)
-db = client['golestandb']
-col = db['user']
-
-counter = 0
-go_back_emoji = "بازگشت ↪"
-
+GO_BACK_EMOJI = "بازگشت ↪"
 with open(os.path.realpath('unis.json')) as f:
     uni_json_list = json.load(f)
 
@@ -33,50 +25,16 @@ def uni_generator():
             yield uni
 
 
-def registered_user(update: Update):
-    username = update.message.from_user.username
-    username_query = dict(telegramUsername=f'{username}')
-    if col.find_one(username_query):
-        return True
+# Commands
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
+
+def help_cmd(update: Update, context: CallbackContext) -> None:
     """Displays info on how to use the bot."""
     update.message.reply_text("The text/img for the help will be here.")
 
 
-def golestan_menu():
-
-    golestan_menu_ = [
-        [InlineKeyboardButton("برنامه هفتگی", callback_data='week_schedule')],
-        [InlineKeyboardButton(go_back_emoji, callback_data='golestan_menu_back')],
-    ]
-    golestan_reply_markup = InlineKeyboardMarkup(golestan_menu_)
-    return golestan_reply_markup
-
-
-def start_menu(update: Update, back=None):
-
-    start_menu_ = [
-        [
-            InlineKeyboardButton("تنظیمات", callback_data='setting'),
-            InlineKeyboardButton("ورود به سامانه گلستان", callback_data='login'),
-        ],
-    ]
-
-    start_menu_markup = InlineKeyboardMarkup(start_menu_)
-    if back:
-        query = update.callback_query
-        query.edit_message_text(text="به سامانه گلستان خوش آمدید!\nبرای ادامه بر روی گزینه مورد نظر کلیک کنید.",
-                                reply_markup=start_menu_markup)
-        return
-    else:
-        update.message.reply_text(text="به سامانه گلستان خوش آمدید!\nبرای ادامه بر روی گزینه مورد نظر کلیک کنید.",
-                                  reply_markup=start_menu_markup)
-
-
 def start_menu_new_user(update: Update, back=None):
-
     start_menu_ = [
         [
             InlineKeyboardButton("راهنما", callback_data='guide'),
@@ -95,8 +53,17 @@ def start_menu_new_user(update: Update, back=None):
                                   reply_markup=start_menu_new_user_markup)
 
 
-def provinces_menu(query, update: Update = None, back=None):
 
+    # if back:
+    #     query = update.callback_query
+    #     query.edit_message_text(text="به سامانه گلستان خوش آمدید!\nبرای ادامه بر روی گزینه مورد نظر کلیک کنید.",
+    #                             reply_markup=start_menu_markup)
+    # else:
+    #     update.message.reply_text(text="به سامانه گلستان خوش آمدید!\nبرای ادامه بر روی گزینه مورد نظر کلیک کنید.",
+    #                               reply_markup=start_menu_markup)
+
+
+def provinces_menu(query, update: Update = None, back=None):
     province_menu = [
         [InlineKeyboardButton("{}".format(list(uni_json_list.keys())[i]),
                               callback_data="{}".format(list(uni_json_list.keys())[i])) for i in
@@ -104,7 +71,7 @@ def provinces_menu(query, update: Update = None, back=None):
     ]
 
     reshaped_province_menu = reshape_menu(province_menu, 2)
-    reshaped_province_menu.append([InlineKeyboardButton(go_back_emoji, callback_data="province_menu_back_btn")])
+    reshaped_province_menu.append([InlineKeyboardButton(GO_BACK_EMOJI, callback_data="province_menu_back_btn")])
     province_reply_markup = InlineKeyboardMarkup(reshaped_province_menu)
 
     if back:
@@ -114,7 +81,6 @@ def provinces_menu(query, update: Update = None, back=None):
 
 
 def unis_menu(query, city=None, back=None):
-
     if back:
         provinces_menu(query)
         return
@@ -126,17 +92,18 @@ def unis_menu(query, city=None, back=None):
              range(0, len(list(uni_json_list.get(city))))]
         ]
         reshaped_uni_menu = reshape_menu(uni_menu, 1)
-        reshaped_uni_menu.append([InlineKeyboardButton(go_back_emoji, callback_data="uni_menu_back_btn")])
+        reshaped_uni_menu.append([InlineKeyboardButton(GO_BACK_EMOJI, callback_data="uni_menu_back_btn")])
         uni_menu_markup = InlineKeyboardMarkup(reshaped_uni_menu)
         query.edit_message_text("انتخاب دانشگاه 🏫", reply_markup=uni_menu_markup)
 
 
-def start(update: Update, context: CallbackContext) -> None:
-
-    if registered_user(update):
-        start_menu(update)
-    else:
-        start_menu_new_user(update)
+def golestan_menu(query):
+    golestan_menu_ = [
+        [InlineKeyboardButton("برنامه هفتگی", callback_data='week_schedule')],
+        [InlineKeyboardButton(GO_BACK_EMOJI, callback_data='golestan_menu_back')],
+    ]
+    golestan_reply_markup = InlineKeyboardMarkup(golestan_menu_)
+    query.edit_message_text(text='انتخاب کنید', reply_markup=golestan_reply_markup)
 
 
 def reshape_menu(button_list: list, column: int):
@@ -149,151 +116,87 @@ def reshape_menu(button_list: list, column: int):
     return button_list
 
 
-# def login():
-#
-#     Browser.get_url(UNIVERSITY_LINK)
-#     Browser.captcha()
-#     Browser.enter_username_password(GOLESTAN_USERNAME, GOLESTAN_PASSWORD)
-#     while True:
-#         try:
-#             Browser.submit_username_password()
-#             Browser.go_to_login_error_frame()
-#             Browser.check_error()
-#         except(NoSuchFrameException, NoSuchWindowException, ElementNotInteractableException):
-#             break
-
 class TelegramBot:
+
     TELEGRAM_USERNAME = ""
-    GOLESTAN_USERNAME = ""
-    GOLESTAN_PASSWORD = ""
     UNIVERSITY_LINK = ""
     UNIVERSITY_NAME = ""
-    CAPTCHA = ""
-    counter = 0
     browser = Browser()
 
-    def button(self, update: Update, context: CallbackContext) -> None:
-        """Parses the CallbackQuery and updates the message text."""
-        query = update.callback_query
-        # Start Inlinekeyboard -> CHOOSE PROVINCE
-        if query.data == 'choose_province_btn':
-            provinces_menu(query)
-        # Choose Province BACK Inlinekeyboard -> Start
-        if query.data == 'province_menu_back_btn':
-            start_menu_new_user(update, back=True)
-        # Province Inlinekeyboard -> CHOOSE UNI
-        for city in list(uni_json_list.keys()):
-            if query.data == city:
-                return unis_menu(query, city)
-        # Choose Uni BACK Inlinekeyboard -> Province
-        if query.data == 'uni_menu_back_btn':
-            unis_menu(query, back=True)
-        # Select Uni
-        uni_name_list = uni_generator()
-        for uni in uni_name_list:
-            for uni_name, link in uni.items():
-                if query.data == uni_name[0:28]:
-                    self.UNIVERSITY_NAME = uni_name
-                    self.UNIVERSITY_LINK = link
-                    self.TELEGRAM_USERNAME = query.from_user.username
-                    query.bot.answer_callback_query(query.id, text='نام دانشگاه ذخیره شد.', show_alert=True,
-                                                    cache_time=0)
-
-                    self.save_to_db()
-                    return start_menu(update, back=True)
-
-        if query.data == "login":
-            query.bot.send_message(text='اطلاعات کاربری خود در سامانه گلستان را وارد کنید.',
-                                   chat_id=query.message.chat_id)
-            query.bot.send_message(query.message.chat_id, 'شناسه کاربری:', reply_markup=ForceReply())
-
-        if query.data == "week_schedule":
-            query.bot.send_message(query.message.chat_id, "لطفا صبر کنید.")
-            self.browser.go_to_menu('78')
-            if os.path.isfile('week.png'):
-                query.bot.send_photo(chat_id=query.message.chat_id, photo=open('week.png', 'rb'))
-            if os.path.isfile('week.txt'):
-                with open('week.txt', 'r') as textFile:
-                    text = textFile.read()
-                query.bot.send_message(chat_id=query.message.chat_id, text=text)
-
-        if query.data == "golestan_menu_back":
-            start_menu(update, back=True)
-
     def save_to_db(self):
-        col.insert_one(dict(telegramUsername=f'{self.TELEGRAM_USERNAME}', uniName=f'{self.UNIVERSITY_NAME}',
-                            uniLink=f'{self.UNIVERSITY_LINK}', golestanUsername=f'{self.GOLESTAN_USERNAME}',
-                            golestanPassword=f'{self.GOLESTAN_PASSWORD}'))
+        Mongo.COL.insert_one({"telegramUsername": "{}".format(self.TELEGRAM_USERNAME),
+                              "uniName": "{}".format(self.UNIVERSITY_NAME),
+                              "uniLink": "{}".format(self.UNIVERSITY_LINK), "golestanUsername": "",
+                              "golestanPassword": ""})
 
-    def login(self, update: Update, context: CallbackContext):
+    def login_to_golestan(self, update: Update, context: CallbackContext):
+
+        self.get_uni_page(update, context)
+        self.captcha(update, context)
+
+    def get_uni_page(self, update: Update, context: CallbackContext):
 
         context.bot.send_message(chat_id=update.message.chat_id, text="لطفا صبر کنید...")
         username = update.message.from_user.username
-        row = col.find_one({"telegramUsername": username})
+        row = Mongo.COL.find_one({"telegramUsername": username})
         self.browser.get_url(row['uniLink'])
+
+    def captcha(self, update: Update, context: CallbackContext):
+
         self.browser.captcha_screenshot()
         context.bot.send_photo(chat_id=update.message.chat_id, photo=open('captcha.png', 'rb'))
         context.bot.send_message(update.message.chat_id, text="کپچا را وارد کنید.")
         context.bot.send_message(update.message.chat_id, text="کپچا:", reply_markup=ForceReply())
 
-    def submit(self, update: Update, context: CallbackContext):
-        self.browser.enter_captcha(self.CAPTCHA)
-        self.browser.enter_username_password(self.GOLESTAN_USERNAME, self.GOLESTAN_PASSWORD)
-        while True:
-            try:
-                self.browser.submit_username_password()
-                self.browser.go_to_login_error_frame()
-                self.browser.check_error()
-            except(NoSuchFrameException, NoSuchWindowException, ElementNotInteractableException):
-                break
-        try:
-            wait(self.browser.driver, 2).until(EC.visibility_of_element_located((By.XPATH,
-                                                "//span[@style='cursor:hand;color:blue']")))
-            col.find_one_and_update(
-                {"telegramUsername": update.message.from_user.username},
-                {"$set": {"golestanUsername": self.GOLESTAN_USERNAME,
-                          "golestanPassword": self.GOLESTAN_PASSWORD}})
-            print('saved')
-
-        except NoSuchElementException:
-            print('didnt work')
-
-        context.bot.send_message(chat_id=update.message.chat_id, text='گزینه مورد نظر را انتخاب کنید:',
-                                 reply_markup=golestan_menu())
-
-    def input_golestan_credentials(self, update: Update, context: CallbackContext):
-        if self.counter == 0:
-            self.GOLESTAN_USERNAME = update.message.text
-            context.bot.send_message(update.message.chat_id, 'گذرواژه:', reply_markup=ForceReply())
-            self.counter += 1
-
-        elif self.counter == 1:
-            self.GOLESTAN_PASSWORD = update.message.text
-            self.counter += 1
-            self.login(update, context)
-
-        elif self.counter == 2:
-            self.CAPTCHA = update.message.text
-            self.counter += 1
-            self.submit(update, context)
 
 
-def main() -> None:
-    """Run the bot."""
-    telegram = TelegramBot()
-    updater = Updater(API_KEY)
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('help', help_command))
-    updater.dispatcher.add_handler(CallbackQueryHandler(telegram.button))
-    updater.dispatcher.add_handler(MessageHandler(Filters.text, telegram.input_golestan_credentials))
-
-    # Start the Bot
-    updater.start_polling()
-
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
-    updater.idle()
 
 
-if __name__ == '__main__':
-    main()
+    # def submit(self, update: Update, context: CallbackContext):
+    #     self.enter_user_password()
+    #     self.enter_captcha()
+    #     while True:
+    #         try:
+    #             self.browser.submit_username_password()
+    #             self.browser.go_to_login_error_frame()
+    #             self.check_error(update, context)
+    #         except(NoSuchFrameException, NoSuchWindowException, ElementNotInteractableException):
+    #             break
+    #     try:
+    #         wait(self.browser.driver, 2).until(EC.visibility_of_element_located((By.XPATH,
+    #                                            "//span[@style='cursor:hand;color:blue']")))
+    #
+    #         Mongo.COL.find_one_and_update(
+    #             {"telegramUsername": update.message.from_user.username},
+    #             {"$set": {"golestanUsername": self.`GOLESTAN_USERNAME`,
+    #                       "golestanPassword": self.GOLESTAN_PASSWORD}})
+    #         print('saved')
+    #
+    #     except NoSuchElementException:
+    #         print("didn't work")
+    #
+    #     context.bot.send_message(chat_id=update.message.chat_id, text='گزینه مورد نظر را انتخاب کنید:',
+    #                              reply_markup=golestan_menu())
+
+
+def save_credentials(update: Update, context: CallbackContext):
+    kb = [
+        [InlineKeyboardButton("بله", callback_data='yes')],
+        [InlineKeyboardButton("خیر", callback_data='no')],
+    ]
+    kb_markup = InlineKeyboardMarkup(kb)
+    context.bot.send_message(chat_id=update.message.chat_id, text="آيا مایلید اطلاعات کاربری شما برای استفاده آتی ذخیره شود؟",
+                             reply_markup=kb_markup)
+
+
+
+
+
+
+
+
+class Menu(Enum):
+    WEEK_SCHEDULE = 78
+    WEEK_SCHEDULE_ENROLLMENT = 88
+    FINAL_EXAMS = 428
+
